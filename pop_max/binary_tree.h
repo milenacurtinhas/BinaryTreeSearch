9588a6c64c43ef80a6ecf165554055506c9c6b38
Milena <116603940+milenacurtinhas@users.noreply.github.com>
2023-08-13 22:39:04 -0300

    Add files via upload

---
+++ pop_max/binary_tree.h
@@ -0,0 +1,47 @@
+
+#ifndef _BINARY_TREE_H_
+#define _BINARY_TREE_H_
+
+typedef int (*CmpFn)(void *, void *);
+typedef void (*KeyDestroyFn)(void *);
+typedef void (*ValDestroyFn)(void *);
+
+typedef struct
+{
+    void *key;
+    void *value;
+} KeyValPair;
+
+typedef struct Node
+{
+    void *key;
+    void *value;
+    struct Node *left;
+    struct Node *right;
+    struct Node *parent;
+} Node;
+
+typedef struct
+{
+    Node *root;
+    CmpFn cmp_fn;
+    KeyDestroyFn key_destroy_fn;
+    ValDestroyFn val_destroy_fn;
+} BinaryTree;
+
+KeyValPair *key_val_pair_construct(void *key, void *val);
+void key_val_pair_destroy(KeyValPair *kvp);
+
+Node *node_construct(void *key, void *value, Node *left, Node *right, Node *parent);
+void node_destroy(Node *node);
+
+BinaryTree *binary_tree_construct(CmpFn cmp_fn, KeyDestroyFn key_destroy_fn, ValDestroyFn val_destroy_fn);
+void binary_tree_add(BinaryTree *bt, void *key, void *value);
+int binary_tree_empty(BinaryTree *bt);
+void binary_tree_remove(BinaryTree *bt, void *key);
+KeyValPair *binary_tree_pop_min(BinaryTree *bt);
+KeyValPair *binary_tree_pop_max(BinaryTree *bt);
+void *binary_tree_get(BinaryTree *bt, void *key);
+void binary_tree_destroy(BinaryTree *bt);
+
+#endif
